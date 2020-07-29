13a67f2ba0bd3764c40c14c4a43422ea41833cb1
“maki5588” <your-mail@example.com>
2020-07-29 19:35:00 +0900

    fix name

--- hatena.py
+++ hatena.py
@@ -18,14 +18,11 @@ class HatenaScraper(object):
         self.channel_videos_url = os.path.join(self.youtube_url, 'search', self.user_name)
         self.titles = []
         self.video_urls = []
-        self.tags = []
+        self.good_counts = []
 
     def run(self):
-        #ソースの取得
         self.get_page_source()
-        #動画とURLの抽出
         self.parse_video_title_and_url()
-        #データの保存
         self.save_as_csv_file()
 
     def get_page_source(self):
@@ -33,19 +30,14 @@ class HatenaScraper(object):
         YoutubeChannelページの
         最下部までスクロールしたページソースを取得
         '''
-        # ブラウザ操作の準備
         self.driver = webdriver.Chrome()
         self.driver.get(self.channel_videos_url)
         self.current_html = self.driver.page_source
 
-        # 動画一覧要素へ移動
-        # element = self.driver.find_element_by_xpath('//*[@class="style-scope ytd-page-manager"]')
         actions = ActionChains(self.driver)
-        # actions.move_to_element(element)
         actions.perform()
         actions.reset_actions()
 
-        # 最下部までスクロールしたソースを取得
         while True:
             for j in range(100):
                 actions.send_keys(Keys.PAGE_DOWN)
@@ -63,76 +55,27 @@ class HatenaScraper(object):
         '''
         soup = BeautifulSoup(self.current_html, 'html.parser')
         for i in soup.find_all("a"):
-            # print(i)
             title = (i.get("title"))
             url = (i.get("href"))
-            tag_x = (i.get("title"))
-            # view_text = (re.findall('前 .*回視聴', view_material))
-            # # 再生回数の材料
-            # view_material_text = (i.get("href=/search/tag?"))
+            good_count_text = (i.get("title"))
             if title is None:
                 continue
             elif url is None:
                 continue
-            elif tag_x is None:
+            elif good_count_text is None:
                 continue
-            tag_x = title.replace('　', ' ')
-            # print(tag_x)
-            tag_y = re.findall('[0-9]+', tag_x)
-            if len(tag_y) > 1:
-                tag = tag_y[-1]
+            good_count_text = title.replace('　', ' ')
+            good_count_int = re.findall('[0-9]+', good_count_text)
+            if len(good_count_int) > 1:
+                good_count = good_count_int[-1]
             else:
-                tag_z = tag_y
-                tag = ','.join(tag_z)
-                print(tag)
-                # tag = tag_y[-1]
-            # print(tag)
-            # tag_y =  tag_x.replace('　', ' ')
-            # print(tag_y)
-            # tag = (re.findall(' (*ブックマーク)', tag_y))
-            # print(tag_y)
-            # tag = title
-            # if 'href="/search/tag?q=' in url:
-            #     print(i)
-            # print(tag)
-            # if "/search/tag?q=" in tag:
-            # print(title)
-            # elif view_material_text is None:
-            #     continue
-            # 再生回数の材料の空白を削除
-            # view_material = view_material_text.replace('　', ' ')
-            # # 文字列中の前から回視聴以外を削除
-            # if view_text:
-            #     # if view_text in ' 秒 ':
-            #     # view_text = (re.findall('前 .*回視聴', view_material))
-            #     string = ",".join(view_text)
-            #     string_new = string.replace('前 ', '')
-            #     view_text_x = string_new.replace(' 回視聴', '')
-            #     view_text_y = view_text_x.replace(',', '')
-            #     # view_text_z = view_text_y.replace(' ', '')
-            #     # str_list_new = view_text_y.split(",")
-            #     # print(str_list_new)
-            #     str_list_new_x = [int(s) for s in view_text_y.split() if s.isdigit()]
-            #     view = (str_list_new_x[-1])
-            #     print(view)
-                # view_text_x.isupper()
-                # view_text.replace('前 ', '')
-                # str_list_new.replace('', '')
-            # print(view_text)
-            # もし、文字列中に秒があった場合
-            # if view_text in ' 秒 ':
-            # False not in [view_material in ' 秒 ' for view_material in ['秒']]
-            # if '秒' in view_text:
-            # 文字列中の秒から回視聴以外を削除
-            # view_text = (re.findall('秒 .*回視聴', view_material))
-            # 数字以外を削除
-            # view_text = str(view_text)
-            # view = (re.sub("\\D", "", view_text))
+                good_count_arry = good_count_int
+                good_count = ','.join(good_count_arry)
+                print(good_count)
             if "/entry/s/" in url:
                 self.titles.append(title)
                 self.video_urls.append(url)
-                self.tags.append(tag)
-                # self.views.append(view)
+                self.good_counts.append(good_count)
 
     def save_as_csv_file(self):
         '''
@@ -141,7 +84,7 @@ class HatenaScraper(object):
         data = {
          "title": self.titles,
          "url": self.video_urls,
-         "tag": self.tags
+         "good_count": self.good_counts
         }
         pd.DataFrame(data).to_csv(self.csv_file_path,index=False)
 
